5a887c700ec4abe92e2b04ccd3094063f7982f48
User <User@LAB2-2>
2024-10-07 15:05:44 +0300

    ghfx

---
+++ ServerReWear/DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerReWear.DTO
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
